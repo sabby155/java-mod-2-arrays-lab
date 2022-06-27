4da6e711bc59bead4de42e58bac152bddc0dc54b
sabby155 <nebula155@gmail.com>
2022-06-27 12:14:10 -0400

    Arrays Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        Scanner studentNameScanner = new Scanner(System.in);
+        String[] studentList = new String[10];
+
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.println("Enter student's name:");
+            String studentName = studentNameScanner.nextLine();
+            studentList[i] = studentName;
+        }
+
+        System.out.println("You have entered the following names:");
+
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.println(studentList[i]);
+        }
     }
 }
